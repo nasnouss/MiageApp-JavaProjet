38134e04173be38062065ed8ebdb7ecc391ffda0
anis <anisbenbrahim@mbp-de-anis>
2015-03-21 03:05:05 +0100

    first commit

---
+++ WhereYouAt/src/RealProject/HostIPDataLoader.java
@@ -0,0 +1,67 @@
+package RealProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+
+
+
+public class HostIPDataLoader implements PointDataLoader{
+	
+	@Override
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean getPointData(Coordonnees point) {
+		URL urlHostIp;
+		try {
+			urlHostIp = new URL("http://api.hostip.info/?ip="+point.getIp());
+			System.out.println("http://api.hostip.info/?ip="+point.getIp());
+			URLConnection connection  = urlHostIp.openConnection();
+			
+			//Create a "parser factory" for creating SAX parsers
+	        SAXParserFactory spfac = SAXParserFactory.newInstance();
+
+	        //Now use the parser factory to create a SAXParser object
+	        SAXParser sp = spfac.newSAXParser();
+
+	        //Create an instance of this class; it defines all the handler methods
+	        HostIPParser handler = new HostIPParser(point);
+
+	        //Finally, tell the parser to parse the input and notify the handler
+	        sp.parse(connection.getInputStream(), handler);
+	        
+	        return handler.isDataOk();
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+       
+
+
+	}
+
+}
